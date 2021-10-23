edb7469d27b72f6639679200e0bd483f23ef3428
Kartik <9415kks@gmail.com>
2021-10-24 02:16:58 +0530

    Completed the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Announcement from '../components/Announcement.jsx'
 import Categories from '../components/Categories.jsx'
+import Footer from '../components/Footer.jsx'
 import Navbar from '../components/Navbar.jsx'
+import Newsletter from '../components/Newsletter.jsx'
 import Products from '../components/Products.jsx'
 import Slider from '../components/Slider.jsx'
 
@@ -13,6 +15,8 @@ const Home = () => {
             <Slider/>
             <Categories/>
             <Products/>
+            <Newsletter/>
+            <Footer/>
         </div>
     )
 }
