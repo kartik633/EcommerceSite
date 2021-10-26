42ac027d3215e53035c4d4dafb32c47ac85e0b5b
Kartik <9415kks@gmail.com>
2021-10-26 23:02:17 +0530

    Shopping Cart Designing in process

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import Home from "./pages/Home.jsx";
 import ProductList from "./pages/ProductList.jsx";
 import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
+import Cart from "./pages/Cart.jsx";
 
 const App = () => {
-  return <Login />
+  return <Cart />
 };
 
 export default App;
