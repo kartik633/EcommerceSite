66d5481a4c4860243c3e6c776503d220068a47d6
Kartik <9415kks@gmail.com>
2021-10-30 15:03:08 +0530

    Completed the UI part of the Website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,11 @@ import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
 import gslobal from '../global.css'
+import { mobile } from '../responsive';
 
 const Container = styled.div`
     height:60px;
+    ${mobile({height:"50px"})}
 `;
 
 const Wrapper = styled.div`
@@ -13,11 +15,14 @@ const Wrapper = styled.div`
     display:flex;
     align-items: center;
     justify-content: space-between;
+    ${mobile({padding:"10px 0px"})}
 `;
 
 const Language = styled.span`
     font-size:14px;
     cursor:pointer;
+    ${mobile({display:"none"})}
+
 `;
 
 const SearchContainer = styled.div`
@@ -26,16 +31,20 @@ const SearchContainer = styled.div`
     align-items: center;
     margin-left: 25px;
     padding:5px;
+    ${mobile({marginLeft:"10px"})}
+
 `;
 
 const Input = styled.input`
     border:none;
+    ${mobile({width:"50px"})}
 `;
 
 const Left = styled.div`
     flex:1;
     display: flex;
     align-items: center;
+    ${mobile({justifyContent:"center"})}
 `;
 const Center = styled.div`
     flex:1;
@@ -44,18 +53,21 @@ const Center = styled.div`
 
 const Logo = styled.h1`
     font-weight: bold;
+    ${mobile({fontSize:"24px"})}
 `;
 const Right = styled.div`
     flex:1;
     display: flex;
     align-items: center;
     justify-content: flex-end;
+    ${mobile({flex:2, justifyContent:"center"})}
 `;
 
 const MenuItem = styled.div`
     font-size: 14px;
     cursor: pointer;
     margin-left: 25px;
+    ${mobile({fontSize:"12px", marginLeft:"10px"})}
 `;
 
 
@@ -70,7 +82,7 @@ const Navbar = () => {
                     </Language>
 
                     <SearchContainer>
-                        <Input/>
+                        <Input placeholder="Search"/>
                         <Search style={{color:"gray",fontsize:16}}/>
                     </SearchContainer>
                 </Left>
